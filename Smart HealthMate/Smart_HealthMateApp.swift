c18d3dd363d10f46959b6bd413a4046570adce1d
Moin UD-din <129534945+MoinUddin-dev-dot@users.noreply.github.com>
2025-06-18 17:06:21 +0500

    Initial Commit

---
+++ Smart HealthMate/Smart_HealthMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_HealthMateApp.swift
+//  Smart HealthMate
+//
+//  Created by Moin on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Smart_HealthMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
